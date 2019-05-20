5f27a69cefe770741d5063bcad83cbbe0c84fce0
Max Aubain <max.aubain@gmail.com>
2019-05-20 17:49:36 +0200

    exercise 1

---
+++ hello_world.rb
@@ -0,0 +1,4 @@
+names = ['Max', 'Samantha', 'Dazz']
+names.each do |names|
+    puts "Hello #{names}"
+end
---
+++ my_group.rb
@@ -0,0 +1,5 @@
+my_group = Array.new
+person_1 = [name: "Max", gender: "M", age: "32"]
+person_2 = [name: "Samantha", gender: "F", age: "34"]
+person_3 = [name: "Dazz", gender: "X", age: "27"]
+my_group = [person_1, person_2, person_3]
