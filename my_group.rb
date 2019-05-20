978391efcc7dbf0c0ae8d5c8d28bb3417774d397
Max Aubain <max.aubain@gmail.com>
2019-05-20 19:44:42 +0200

    Bug fix [] to {} and do loop

--- my_group.rb
+++ my_group.rb
@@ -1,5 +1,12 @@
 my_group = Array.new
-person_1 = [name: "Max", gender: "M", age: "32"]
-person_2 = [name: "Samantha", gender: "F", age: "34"]
-person_3 = [name: "Dazz", gender: "X", age: "27"]
+
+# key:hash needs {}, arrays need []
+person_1 = {name: "Max", gender: "man", age: "32"}
+person_2 = {name: "Samantha", gender: "woman", age: "34"}
+person_3 = {name: "Dazz", gender: "gender non-binary person", age: "27"}
 my_group = [person_1, person_2, person_3]
+
+# do loop for each my_group variable, variable called "person", print the following sentence...
+my_group.each do |person|
+    puts "#{person[:name]} is a #{person[:age]} year old #{person[:gender]}."
+end
